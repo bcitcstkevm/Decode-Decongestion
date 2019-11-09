77ce8a53a9bcdc2e408a7631a176b6c04c0cba55
Derrick Lee <derricklee91@gmail.com>
2019-11-09 08:28:57 -0800

    UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,90 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api'
 import Head from 'next/head'
-import { displayPano } from '../scripts/pannellum'
+import { displayPano } from '../scripts/pannellum';
+import { Button, makeStyles, TextField, } from '@material-ui/core';
+import styled from 'styled-components';
 
-export default class Index extends React.Component {
-  componentDidMount() {
-    displayPano('https://pannellum.org/images/alma.jpg')
-  }
+const Root = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
-  render() {
-    return <div style={{display: 'flex', height: '100vh'}}>
+const useStyles = makeStyles({
+  button: {
+
+  },
+});
+
+export default function Index() {
+  useEffect(() => {
+    // displayPano('https://pannellum.org/images/alma.jpg');
+  }, [])
+  return (
+    <div>
       <Head>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
-        <script src="https://cdn.jsdelivr.net/npm/pannellum@2.5.4/build/pannellum.js"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.4/build/pannellum.css"></link>
-      </Head>
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+         <script src="https://cdn.jsdelivr.net/npm/pannellum@2.5.4/build/pannellum.js"></script>
+         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.4/build/pannellum.css"></link>
+       </Head>
 
-      <div style={{display: 'flex', flex: 1}}>
-        <button className="btn btn-success">asd</button>
-        <div id="panorama"></div>
-      </div>
-
-      <div style={{display: 'flex', flex: 1}}>
-        <LoadScript
-          id="script-loader"
-          googleMapsApiKey="AIzaSyAdgZKoxkdZjb92G7aMvEiJYiegd9n6rbA"
-        >
-          <GoogleMap
-            id='example-map'
-            mapContainerStyle={{ height: '100%', width: '100%' }}
-            center={{lat: 49.2577143, lng: -123.1939432}}
-            zoom={12}
-          >
-          </GoogleMap>
-        </LoadScript>
-      </div>
+       <Button>Test</Button>
+       {/* <TextField /> */}
     </div>
-  }
+    // <Root>
+    //   <Head>
+    //     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+    //     <script src="https://cdn.jsdelivr.net/npm/pannellum@2.5.4/build/pannellum.js"></script>
+    //     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.4/build/pannellum.css"></link>
+    //   </Head>
+
+    //   <div style={{display: 'flex', flex: 1}}>
+    //     <div id="panorama"></div>
+    //   </div>
+    //   {/* <div>
+    //     <Button>Test</Button>
+    //     <TextField placeholder="Hello" />
+    //   </div> */}
+      
+    // </Root>
+      
+
+  );
 }
+
+// export default class Index extends React.Component {
+//   componentDidMount() {
+//     displayPano('https://pannellum.org/images/alma.jpg')
+//   }
+
+//   render() {
+//     return <div style={{display: 'flex', height: '100vh'}}>
+//       <Head>
+//         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+//         <script src="https://cdn.jsdelivr.net/npm/pannellum@2.5.4/build/pannellum.js"></script>
+//         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.4/build/pannellum.css"></link>
+//       </Head>
+
+//       <div style={{display: 'flex', flex: 1}}>
+//         <button className="btn btn-success">asd</button>
+//         <div id="panorama"></div>
+//       </div>
+
+//       <div style={{display: 'flex', flex: 1}}>
+//         <LoadScript
+//           id="script-loader"
+//           googleMapsApiKey="AIzaSyAdgZKoxkdZjb92G7aMvEiJYiegd9n6rbA"
+//         >
+//           <GoogleMap
+//             id='example-map'
+//             mapContainerStyle={{ height: '100%', width: '100%' }}
+//             center={{lat: 49.2577143, lng: -123.1939432}}
+//             zoom={12}
+//           >
+//           </GoogleMap>
+//         </LoadScript>
+//       </div>
+//     </div>
+//   }
+// }
