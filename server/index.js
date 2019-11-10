4feda091093f24425bea52cf53a397ecd4009e33
Browne <kyle.browne@move.com>
2019-11-09 20:06:37 -0800

    hosting files changed to heroku port

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ app
     });
 
 
-    server.listen(3000, (err) => {
+    server.listen(process.env.PORT, (err) => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
     });
