b64b226adaea963054eae0d2d19bb9e74da65b39
Allan Hoang <alhg@users.noreply.github.com>
2019-11-09 17:26:54 -0800

    interpolate lattng (#11)

--- src/components/map/streetview.js
+++ src/components/map/streetview.js
@@ -4,7 +4,7 @@ import { StreetViewPanorama } from '@react-google-maps/api';
 function StreetView(props) {
   return (
     <StreetViewPanorama
-      pov={{ heading: props.heading, pitch: 0 }}
+      pov={{ heading: props.positions[props.currentPosition].heading, pitch: 0 }}
       position={props.positions[props.currentPosition]}
       visible={props.visbility}
       motionTracking={true}
